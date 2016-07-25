68ed0bfb71e7b2a35e8e9d1d85a4592d16622ff6
rowana16 <aaron.rowan@gmail.com>
2016-07-25 12:33:18 -0400

    Initial Commit - Complete Prior to Blog

---
+++ PortfolioV4/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CFPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Skills()
+        {
+            return View();
+        }
+
+        public ActionResult Week1()
+        {
+            return View();
+        }
+
+        public ActionResult Week1exercises()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioV4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PortfolioV4.MvcApplication" Language="C#" %>
