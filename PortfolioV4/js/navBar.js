e9f94f3bbdb8636d65f04bf9d0ca5abb8a7dcfa1
rowana16 <aaron.rowan@gmail.com>
2016-07-26 13:22:14 -0400

    Active Navbar

---
+++ PortfolioV4/js/navBar.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    var url = window.location.pathname;
+    var substr = url.split('/');
+    var urlaspx = substr[substr.length - 1];
+    if (urlaspx === "") { urlaspx = 'index';}
+    $('.nav').find('.active').removeClass('active');
+    $('.nav li a').each(function () {
+        if (this.href.indexOf(urlaspx) >= 0) {
+            $(this).parent().addClass('active');
+        }
+    });
+});
